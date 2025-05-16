5faefd17591e62fc83c2cc0acd0b6c2f5ca10246
vvisee <visellimichele2@gmail.com>
2025-05-16 12:31:04 +0200

    Add Ristorante.java
    aggiunta classe per ristoranti

---
+++ the_knife/src/main/java/the_knife/classes/Ristorante.java
@@ -0,0 +1,82 @@
+package the_knife.classes;
+
+public class Ristorante {
+    String nome;
+    String indirizzo;
+    String nazione;
+    String citta;
+    int prezzo;
+    String cucina;
+    double latitudine;
+    double longitudine;
+    String telefono; // per delivery
+    String weburl; // per prenotazione online
+
+    public Ristorante(String nome, String indirizzo, String nazione, String citta, int prezzo, String cucina, double latitudine, double longitudine, String telefono, String weburl) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.nazione = nazione;
+        this.citta = citta;
+        this.prezzo = prezzo;
+        this.cucina = cucina;
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
+        if (weburl == null || weburl.isEmpty()) {
+            this.weburl = "N/A";
+        } else {
+            this.weburl = weburl;
+        }
+        if (telefono == null || telefono.isEmpty()) {
+            this.telefono = "N/A";
+        } else {
+            this.telefono = telefono;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+    public String getNazione() {
+        return nazione;
+    }
+    public String getCitta() {
+        return citta;
+    }
+    public int getPrezzo() {
+        return prezzo;
+    }
+    public String getCucina() {
+        return cucina;
+    }
+    public double getLatitudine() {
+        return latitudine;
+    }
+    public double getLongitudine() {
+        return longitudine;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public String getWeburl() {
+        return weburl;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\n" +
+                "Indirizzo: " + indirizzo + "\n" +
+                "Nazione: " + nazione + "\n" +
+                "Citta: " + citta + "\n" +
+                "Prezzo: " + prezzo + "\n" +
+                "Cucina: " + cucina + "\n" +
+                "Latitudine: " + latitudine + "\n" +
+                "Longitudine: " + longitudine + "\n" +
+                "Telefono: " + telefono + "\n" +
+                "Weburl: " + weburl + "\n";   
+    }
+    
+}
+
