98f744b3133d18b63c7b1be8e0c44f9a12b2047f
GabriMa8 <gabryelemacchi@gmail.com>
2025-05-30 15:23:26 +0200

    Create Login dynamic and functions with verify

--- the_knife/src/main/java/the_knife/LoginController.java
+++ the_knife/src/main/java/the_knife/LoginController.java
@@ -3,6 +3,11 @@ package the_knife;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import the_knife.classes.Utente;
+import java.util.List;
+import java.util.ArrayList;
 
 public class LoginController {
 
@@ -16,7 +21,40 @@ public class LoginController {
     }
 
     @FXML
+    private TextField N_Utente;
+
+    @FXML
+    private PasswordField N_Password;
+
+    @FXML
     private void switchToHome() throws IOException {
-        App.setRoot("Home");
+        String username = N_Utente.getText();
+        String password = N_Password.getText();
+
+        List<?> objects = (List<?>) FileMenager.readFromFile("Utenti.bin");
+        List<Utente> utenti = new ArrayList<>();
+        for (Object obj : objects) {
+            if (obj instanceof Utente) {
+                utenti.add((Utente) obj);
+            }
+        }
+
+        boolean found = false;
+        for (Utente utente : utenti) {
+            if (utente.getUsername().equals(username) && utente.getPassword().equals(password)) {
+                found = true;
+                break;
+            }
+        }
+
+        if(found) {
+            App.setRoot("Home");
+        } else {
+            System.out.println("username o password errati.");
+            N_Utente.clear();
+            N_Password.clear();
+            N_Utente.setStyle("-fx-border-color: red;");
+            N_Password.setStyle("-fx-border-color: red;");
+        }
     }
 }
