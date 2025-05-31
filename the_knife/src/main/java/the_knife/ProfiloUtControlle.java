59017b5b2ed09e16baf56c75575dfee3f8c9d4d7
GabriMa8 <gabryelemacchi@gmail.com>
2025-05-31 11:33:00 +0200

    add visualizzazione dati in profilo ut e profilo ristoratore

--- the_knife/src/main/java/the_knife/ProfiloUtControlle.java
+++ the_knife/src/main/java/the_knife/ProfiloUtControlle.java
@@ -4,11 +4,27 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import the_knife.classes.Utente;
 
 public class ProfiloUtControlle {
     
     Utente u=new Utente();
+
+    @FXML
+    private TextField nome;
+    @FXML
+    private TextField username;
+    @FXML
+    private DatePicker datan;
+    @FXML
+    private TextField cognome;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private TextField luogo;
 
     @FXML
     private void switchToHome() throws IOException {
@@ -17,5 +33,11 @@ public class ProfiloUtControlle {
 
     public void initData(Utente u) {
         this.u = u;
+        nome.setText(u.getNome());
+        cognome.setText(u.getCognome());
+        username.setText(u.getUsername());
+        datan.setValue(u.getDataNascita());
+        password.setText(u.getPassword());
+        luogo.setText(u.getLuogoDomicilio());
     }
 }
